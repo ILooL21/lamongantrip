0856477f065215a0f2a9571501f00f4f2ba5b83e
Muhammad Kholilur Rohman <muhammadkholilurrohman72@gmail.com>
2025-06-16 02:25:30 +0700

    test3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { Notifications } from "react-push-notification";
 
 function App() {
   return (
     <>
-      <Notifications />
       <Outlet />
     </>
   );
